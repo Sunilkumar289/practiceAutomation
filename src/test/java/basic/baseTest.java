ff9bc3de747dc6b34ea0ab6d5333bb4dfcc12d9b
Sunilkumar289 <akssunilsalunke@gmail.com>
2026-06-11 22:44:28 +0530

    remote driver chaanges

--- src/test/java/basic/baseTest.java
+++ src/test/java/basic/baseTest.java
@@ -2,11 +2,15 @@ package basic;
 
 
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.DataProvider;
@@ -18,9 +22,16 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class baseTest {
 	
 	@Test
-	public void luanchApp() {
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver =new ChromeDriver();
+	public void luanchApp() throws MalformedURLException {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless=new");       // modern headless mode
+		options.addArguments("--no-sandbox");         // recommended in containers
+		options.addArguments("--disable-dev-shm-usage"); // avoids /dev/shm issues in Docker
+
+		WebDriver driver = new RemoteWebDriver(
+		    new URL("http://selenium-chrome:4444/wd/hub"),
+		    options
+		);
 		driver.get("http://www.google.com");
 		WebDriverWait wait =new WebDriverWait(driver,Duration.ofSeconds(30));
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Gmail"))).click();
