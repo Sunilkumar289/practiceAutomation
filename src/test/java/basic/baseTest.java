8942991ff6283f688d33c63e36493e8035d21cb7
Sunilkumar289 <akssunilsalunke@gmail.com>
2026-07-06 18:51:17 +0530

    baseTestChanges

--- src/test/java/basic/baseTest.java
+++ src/test/java/basic/baseTest.java
@@ -1,68 +1,42 @@
 package basic;
 
-
-
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.time.Duration;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-
 public class baseTest {
-	
-	public static WebDriver driver;
-	
-	@Test
-	public void luanchApp() throws MalformedURLException {
-		String Execution = System.getProperty("execution");
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--headless=new");       // modern headless mode
-		options.addArguments("--no-sandbox");         // recommended in containers
-		options.addArguments("--disable-dev-shm-usage"); // avoids /dev/shm issues in Docker
+    protected WebDriver driver;
 
-		if(Execution.equalsIgnoreCase("remote"))
-		{
-			
-			driver = new RemoteWebDriver(
-				    new URL("http://selenium-chrome:4444/wd/hub"),
-				    options
-				);
-		}
-		else
-		{
-			WebDriverManager.chromedriver().setup();
-			driver=new ChromeDriver();
-		}
-			
-		
-		driver.get("http://www.google.com");
-		WebDriverWait wait =new WebDriverWait(driver,Duration.ofSeconds(30));
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Gmail"))).click();
-		// wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Sign in']")));
-		System.out.println(driver.getCurrentUrl());
-		driver.navigate().back();
-		System.out.println(driver.getCurrentUrl());
-		System.out.flush();
-		driver.close();
-	   
-		
-	}
-	
-	@DataProvider(name="userDetails")
-	public String[][]  data()
-	{
-		return new String [][]{{"sunil","salunke"},{"Yadhavi","salunke"}};
-	}
+    @BeforeClass
+    public void setUp() throws MalformedURLException {
+        String Execution = System.getProperty("execution", "local");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
 
+        if (Execution.equalsIgnoreCase("remote")) {
+            driver = new RemoteWebDriver(
+                new URL("http://selenium-chrome:4444/wd/hub"),
+                options
+            );
+        } else {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver(options);
+        }
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit(); // closes all windows and ends session cleanly
+        }
+    }
 }
